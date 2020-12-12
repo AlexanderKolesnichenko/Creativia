163a741821fc90f7b0ddd62c0e7270f9d48c2c0c
Alexander Kolesnichenko <kolesnichenkoalex95@gmail.ru>
2020-12-12 17:17:27 +0300

    menu-adaptive

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () { // запуск функции только после полной загрузки контента страницы
 
 
+    $('.menu__burger').on('click', function () {
+        $('.header__menu').toggleClass('active');
+    });
+
 
 
 }, false);
