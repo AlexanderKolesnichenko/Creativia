3e3c4d68fe79f8d5874730ebaba379ad66865200
Alexander Kolesnichenko <kolesnichenkoalex95@gmail.ru>
2020-12-13 12:38:55 +0300

    ready header

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () { // запуск функции только после полной загрузки контента страницы
 
+    //=======Меню бургер========
+    $(window).resize(function() {
+        if ($(window).width() > 768) {
+             $('.header__menu').css("display", "flex");
+        }
+        else{
+            $('.header__menu').css("display", "none");
+
+        }
+    });
 
     $('.menu__burger').on('click', function () {
-        $('.header__menu').toggleClass('active');
+        $('.header__menu').toggle('100');
     });
+//=======Меню бургер========
 
 
 
