1001433130568fa47596bcc6502e298e0e3c28b5
Alexander Kolesnichenko <kolesnichenkoalex95@gmail.ru>
2020-12-15 12:10:09 +0300

    ready about

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () { // запуск функции только после полной загрузки контента страницы
 
     //=======Меню бургер========
-    $(window).resize(function() {
+    $(window).resize(function () {
         if ($(window).width() > 768) {
-             $('.header__menu').css("display", "flex");
-        }
-        else{
+            $('.header__menu').css("display", "flex");
+        } else {
             $('.header__menu').css("display", "none");
 
         }
@@ -15,7 +14,6 @@ document.addEventListener('DOMContentLoaded', function () { // запуск фу
         $('.header__menu').toggle('100');
     });
 //=======Меню бургер========
-
 
 
     //слайдер//
@@ -42,7 +40,6 @@ document.addEventListener('DOMContentLoaded', function () { // запуск фу
 //слайдер//
 
 
-
 //стрелки слайдера//
 
     $('.owl-prev').append(' <svg class="slider-arrow">\n' +
@@ -60,24 +57,16 @@ document.addEventListener('DOMContentLoaded', function () { // запуск фу
     //toggles//
 
 
-
     $('.toggles__block').find(".toggles__topBlock").on('click', function () {
         if ($(this).closest('.toggles__block').hasClass('active')) {
             $(this).closest('.toggles__block').removeClass('active');
-            $(this).closest('.toggles__block').find(".toggles__name").css('color', '#262626');
             $(this).closest('.toggles__block').find(".toggles__textBlock").toggle('100');
-            $(this).closest('.toggles__block').find(".toggles__icon_more").css('display', 'block');
-            $(this).closest('.toggles__block').find(".toggles__icon_less").css('display', 'none');
+
         } else {
             $(this).closest('.toggles__block').addClass('active');
             $(this).closest('.toggles__block').find(".toggles__textBlock").toggle('100');
-            $(this).closest('.toggles__block').find(".toggles__name").css('color', '#ff0036');
-            $(this).closest('.toggles__block').find(".toggles__icon_less").css('display', 'block');
-            $(this).closest('.toggles__block').find(".toggles__icon_more").css('display', 'none');
-
         }
     });
-
 
 
     //toggles//
